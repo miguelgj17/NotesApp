a06fe63cd20dbaf3a4de422c153efebbcf4a6b5c
Lucía <luciapiedrabuena1@gmail.com>
2019-04-13 21:46:54 +0200

    added admin to database

--- Notes/db/schema.rb
+++ Notes/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190320110406) do
+ActiveRecord::Schema.define(version: 20190413193335) do
 
   create_table "notes", force: :cascade do |t|
     t.text     "text"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20190320110406) do
     t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "admin"
   end
 
 end
